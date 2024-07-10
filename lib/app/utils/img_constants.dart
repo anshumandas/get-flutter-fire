255019f945da70574f0ecfbd0252891105c69080
Hardik <hardik@fitpage.in>
2024-07-11 03:13:10 +0530

    fix: correct path typo from 'assets/image' to 'assets/images'

--- lib/app/utils/img_constants.dart
+++ lib/app/utils/img_constants.dart
@@ -1,5 +1,5 @@
 abstract class ImgConstants {
-  static const _assetsImg = 'assets/image';
+  static const _assetsImg = 'assets/images';
 
   static const dash = '$_assetsImg/dash.png';
   static const flutterfire = '$_assetsImg/flutterfire_300x.png';
