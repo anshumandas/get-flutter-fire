6c28b56e4f821b2a4a0f692b8fc9929ddd0df257
Sami0402 <samikhan0402@gmail.com>
2024-07-26 15:21:44 +0530

    Fix: Ensure search icon visibility only on Home page

--- lib/app/modules/root/views/root_view.dart
+++ lib/app/modules/root/views/root_view.dart
@@ -16,38 +16,49 @@ class RootView extends GetView<RootController> {
   final RxBool isSearchBarVisible = false.obs;
   final RxList<Team> searchResults = <Team>[].obs;
 
-  
-
   @override
   Widget build(BuildContext context) {
     return GetRouterOutlet.builder(
       builder: (context, delegate, current) {
         final title = current!.currentPage!.title;
+        final isHomePage = current.currentPage!.name == Routes.HOME;
         return Scaffold(
           key: controller.scaffoldKey,
           drawer: const DrawerWidget(),
           appBar: AppBar(
-            title: Text(title ?? ''),
-            centerTitle: true,
-            leading: GetPlatform.isIOS // Since Web and Android have back button
-                    &&
-                    current.locationString.contains(RegExp(r'(\/[^\/]*){3,}'))
-                ? BackButton(
-                    onPressed: () =>
-                        Get.rootDelegate.popRoute(), //Navigator.pop(context),
-                  )
-                : IconButton(
-                    icon: ImageIcon(
-                      const AssetImage(IconConstants.logo),
-                      color: Colors.grey.shade800,
+              title: Text(title ?? ''),
+              centerTitle: true,
+              leading: GetPlatform
+                          .isIOS // Since Web and Android have back button
+                      &&
+                      current.locationString.contains(RegExp(r'(\/[^\/]*){3,}'))
+                  ? BackButton(
+                      onPressed: () =>
+                          Get.rootDelegate.popRoute(), //Navigator.pop(context),
+                    )
+                  : IconButton(
+                      icon: ImageIcon(
+                        const AssetImage(IconConstants.logo),
+                        color: Colors.grey.shade800,
+                      ),
+                      onPressed: () => AuthService.to.isLoggedInValue
+                          ? controller.openDrawer()
+                          : {Screen.HOME.doAction()},
                     ),
-                    onPressed: () => AuthService.to.isLoggedInValue
-                        ? controller.openDrawer()
-                        : {Screen.HOME.doAction()},
+              actions: [
+                if (isHomePage)
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {
+                      isSearchBarVisible.value = !isSearchBarVisible.value;
+                    },
                   ),
-            actions: topRightMenuButtons(current),
-            // automaticallyImplyLeading: false, //removes drawer icon
-          ),
+                Container(
+                    margin: const EdgeInsets.only(right: 15),
+                    child: Screen.LOGIN.widget(current))
+              ]
+              // automaticallyImplyLeading: false, //removes drawer icon
+              ),
           body: Stack(children: [
             GetRouterOutlet(
               initialRoute: AppPages.INITIAL,
@@ -58,7 +69,7 @@ class RootView extends GetView<RootController> {
             ),
             //
             Obx(() {
-              if (isSearchBarVisible.value) {
+              if (isSearchBarVisible.value && isHomePage) {
                 return buildSearchBar();
               } else {
                 return SizedBox
@@ -156,20 +167,21 @@ class RootView extends GetView<RootController> {
   }
 
 //This could be used to add icon buttons in expanded web view instead of the context menu
-  List<Widget> topRightMenuButtons(GetNavConfig current) {
-    return [
-      //Search Button
-      IconButton(
-        icon: Icon(Icons.search),
-        onPressed: () {
-          isSearchBarVisible.value = !isSearchBarVisible.value;
-        },
-      ),
-      Container(
-          margin: const EdgeInsets.only(right: 15),
-          child: Screen.LOGIN.widget(current))
-    ]; //TODO add seach button
-  }
+  // List<Widget> topRightMenuButtons(GetNavConfig current, bool isHomePage) {
+  //   return [
+  //     //Search Button
+  //     if (isHomePage)
+  //       IconButton(
+  //         icon: Icon(Icons.search),
+  //         onPressed: () {
+  //           isSearchBarVisible.value = !isSearchBarVisible.value;
+  //         },
+  //       ),
+  //     Container(
+  //         margin: const EdgeInsets.only(right: 15),
+  //         child: Screen.LOGIN.widget(current))
+  //   ]; //TODO add seach button
+  // }
 }
 
 List<Team> searcht(String query) {
