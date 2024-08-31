4dc77132c6bdab55a87e8f98b4be1c52eec4febb
JSGulhane <jinalsgulhane@gmail.com>
2024-08-31 20:14:12 +0530

    Attractive UI changes

--- lib/app/modules/products/views/products_view.dart
+++ lib/app/modules/products/views/products_view.dart
@@ -1,11 +1,12 @@
-// ignore_for_file: inference_failure_on_function_invocation
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import '../../../../models/role.dart';
 import '../../../routes/app_pages.dart';
 import '../controllers/products_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+ import '../controllers/products_controller.dart';
+  import '../../../../models/product.dart';
 
 class ProductsView extends GetView<ProductsController> {
   const ProductsView({super.key});
@@ -14,44 +15,79 @@ class ProductsView extends GetView<ProductsController> {
   Widget build(BuildContext context) {
     var arg = Get.rootDelegate.arguments();
     return Scaffold(
-      floatingActionButton:
-          (arg != null && Get.rootDelegate.arguments()["role"] == Role.seller)
-              ? FloatingActionButton.extended(
-                  onPressed: controller.loadDemoProductsFromSomeWhere,
-                  label: const Text('Add'),
-                )
-              : null,
-      body: Column(
-        children: [
-          const Hero(
-            tag: 'heroLogo',
-            child: FlutterLogo(),
+      appBar: AppBar(
+        title: const Text('Products'),
+        backgroundColor: Colors.orange,
+        elevation: 0,
+      ),
+      floatingActionButton: (arg != null && Get.rootDelegate.arguments()["role"] == Role.seller)
+          ? FloatingActionButton.extended(
+              onPressed: controller.loadDemoProductsFromSomeWhere,
+              label: const Text('Add Product'),
+              backgroundColor: Colors.orange,
+              icon: const Icon(Icons.add),
+            )
+          : null,
+      body: Obx(
+        () => RefreshIndicator(
+          onRefresh: () async {
+            controller.products.clear();
+            controller.loadDemoProductsFromSomeWhere();
+          },
+          child: ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemCount: controller.products.length,
+            itemBuilder: (context, index) {
+              final item = controller.products[index];
+              return _buildProductCard(item);
+            },
           ),
-          Expanded(
-            child: Obx(
-              () => RefreshIndicator(
-                onRefresh: () async {
-                  controller.products.clear();
-                  controller.loadDemoProductsFromSomeWhere();
-                },
-                child: ListView.builder(
-                  itemCount: controller.products.length,
-                  itemBuilder: (context, index) {
-                    final item = controller.products[index];
-                    return ListTile(
-                      onTap: () {
-                        Get.rootDelegate.toNamed(Routes.PRODUCT_DETAILS(
-                            item.id)); //we could use Get Parameters
-                      },
-                      title: Text(item.name),
-                      subtitle: Text(item.id),
-                    );
-                  },
-                ),
-              ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProductCard(Product item) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16.0),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: ListTile(
+          contentPadding: const EdgeInsets.all(16.0),
+          leading: CircleAvatar(
+            backgroundColor: Colors.orange.shade100,
+            child: Icon(
+              Icons.local_cafe,
+              size: 40,
+              color: Colors.brown.shade700,
             ),
           ),
-        ],
+          title: Text(
+            item.name,
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.brown.shade700,
+            ),
+          ),
+          subtitle: Text(
+            item.id,
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.brown.shade500,
+            ),
+          ),
+          onTap: () {
+            Get.rootDelegate.toNamed(Routes.PRODUCT_DETAILS(item.id));
+          },
+          tileColor: Colors.orange.shade50,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
       ),
     );
   }
